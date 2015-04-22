f9e480cdcae42f41cac447b106f39a50d435223b
Liang Wang <ryanrhymes@gmail.com>
2015-04-22 21:23:50 +0100

    edit ...

--- supporters.php
+++ supporters.php
@@ -17,18 +17,14 @@ ACM DEV 2015 thanks the following supporters for their generous support!
 </div>
 
 <div>
-<h2 class="hcls">Gold</h2>
-</div>
+<h2 class="hcls">Supporters</h2>
 
-<div>
-<h2 class="hcls">Silver</h2>
-<a href="http://www.internetsociety.org/"><img src="images/isoc.jpg" alt="" style="width: 400px;" /></a>
-</div>
+<a href="http://www.internetsociety.org/"><img src="images/isoc.jpg" alt="" style="width: 250px;" /></a>
 
+<a href="http://www.interdigital.com/"><img src="images/interdigital.jpg" alt="" style="width: 250px;" /></a>
 
-<div>
-<h2 class="hcls">Bronze</h2>
-<a href="http://www.interdigital.com/"><img src="images/interdigital.jpg" alt="" style="width: 400px;" /></a>
+<a href="http://research.microsoft.com/"><img src="images/microsoftresearch.png" alt="" style="width: 250px;" /></a>
+
 </div>
 
 </div>
